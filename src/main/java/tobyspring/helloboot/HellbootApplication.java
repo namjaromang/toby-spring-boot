5cbe97946b43a895e723b810c7c6602789688bb7
namjaromang <dxscf@me.com>
2023-07-11 23:32:19 +0900

    feat. 스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HellbootApplication.java
+++ src/main/java/tobyspring/helloboot/HellbootApplication.java
@@ -1,16 +1,8 @@
 package tobyspring.helloboot;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -19,18 +11,21 @@ public class HellbootApplication {
 
   public static void main(String[] args) {
 //    SpringApplication.run(HellbootApplication.class, args);
-    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+      @Override
+      protected void onRefresh() {
+        super.onRefresh();
+        TomcatServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
+          servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)
+          ).addMapping("/*");
+        });
+        webServer.start();
+      }
+    };
     applicationContext.registerBean(HelloController.class);
     applicationContext.registerBean(HelloService.class);
     applicationContext.refresh();
-
-    TomcatServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
-    WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-      servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext)
-
-          ).addMapping("/*");
-    });
-    webServer.start();
   }
 
 }
