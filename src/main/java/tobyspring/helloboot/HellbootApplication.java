6343d6edd6b1cc5c52e4517e94e9cb846c7effcf
namjaromang <dxscf@me.com>
2023-07-12 00:02:13 +0900

    feat. bean의 생명주기

--- src/main/java/tobyspring/helloboot/HellbootApplication.java
+++ src/main/java/tobyspring/helloboot/HellbootApplication.java
@@ -2,6 +2,7 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +13,32 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class HellbootApplication {
 
+
+  @Bean
+  public ServletWebServerFactory servletWebServerFactory() {
+    return new TomcatServletWebServerFactory();
+  }
+
+
+  @Bean
+  public DispatcherServlet dispatcherServlet() {
+    return new DispatcherServlet();
+  }
+
+
   public static void main(String[] args) {
 //    SpringApplication.run(HellbootApplication.class, args);
     AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
       @Override
       protected void onRefresh() {
         super.onRefresh();
-        TomcatServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+
+        ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
+        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
         WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)
-          ).addMapping("/*");
+          servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+              .addMapping("/*");
         });
         webServer.start();
       }
